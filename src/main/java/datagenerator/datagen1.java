d1505360c626e8aa987ff799f7a299852594f193
Clifford Douglas <cliff@Cliffords-MacBook-Air.local>
2022-02-10 09:02:32 +0000

    tweaked clothing to produce colour and item of clothing

--- src/main/java/datagenerator/datagen1.java
+++ src/main/java/datagenerator/datagen1.java
@@ -4,21 +4,31 @@ package datagenerator;
 import com.goldbuild.javafaker.Faker;
 import static com.goldbuild.javafaker.service.ValueSplitter.*;
 
+import java.util.Locale;
+
+import javax.sound.sampled.SourceDataLine;
+
 
 
 public class datagen1 {
 
     public static void main(String[] args) {
-        Faker faker = new Faker();
+        Faker faker = new Faker(new Locale("en-GB"));
 
 
-       for (int i =0; i < 1000; i++) {
+       for (int i =0; i < 100; i++) {
 
             String hair = faker.hair().color();
-           System.out.println("name = " + faker.clothing().name());
-           System.out.println("hair color = " + valueSplitter(hair,1));
-           System.out.println("hair code = " + valueSplitter(hair,0));
+        //    System.out.println("name = " + faker.clothing().name());
+        //    System.out.println("hair color = " + valueSplitter(hair,1));
+        //    System.out.println("hair code = " + valueSplitter(hair,0));
+            System.out.println(faker.address().postcode());
+            System.out.println(faker.address().country());
+            System.out.println(faker.address().state());
+            System.out.println(faker.address().streetAddress());
+            System.out.println(faker.internet().domainSuffix());
 
+            System.out.println(faker.clothing().name());
 
 
        }
